8f8aec68b71d14d0ff1baf6aaa54c4770e543b28
nithyashree741 <2326kb32@kgcas.com>
2025-06-12 15:27:40 +0530

    Update Task3.java

--- Day6/Task3.java
+++ Day6/Task3.java
@@ -5,7 +5,7 @@ public class Task3 {
 	public static void main(String[] args) {
 		HashSet<String>studentmarks=new HashSet();
         email.add("keerthi@gmail.com");
-        email.add("Divya@gmail.com");
+        email.add("divya@gmail.com");
 		for(String data:email){
 			System.out.println(data);
   
